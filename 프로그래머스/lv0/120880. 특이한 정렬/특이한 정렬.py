307a733a9d2354d9e90347f39c53649f7c8fcfd4
dabin1105 <127091955+dabin1105@users.noreply.github.com>
2023-03-07 00:49:21 +0900

    [level 0] Title: 특이한 정렬, Time: 0.99 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -2,11 +2,11 @@ def solution(numlist, n):
     answer = []
     if n in numlist:
         answer.append(n)
-    for i in range(10000):
-        b = n+(i+1)
-        c = n-(i+1)
-        if b in numlist:
-            answer.append(b)
-        if c in numlist:
-            answer.append(c)
+    for i in range(1,10000):
+        if n +i in numlist:
+            answer.append(n +i)
+
+        if n - i in numlist:
+            answer.append(n -i)
+
     return answer
